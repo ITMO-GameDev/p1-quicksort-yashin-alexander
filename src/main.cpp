0ca94f93cd3c7b89ce52f6ed1fbf153389e6bf40
Alexander Yashin <alexandr.yashin@emlid.com>
2019-10-13 00:19:45 +0300

    repository: setup initial layout

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "salolib/include/quicksort.hpp"
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello, World!" << quicksort() <<std::endl;
+	return 0;
+}
---
+++ src/salolib/src/quicksort.cpp
@@ -0,0 +1,4 @@
+int quicksort()
+{
+      return 0;
+}
