f2f31848dabecaac4945c04f4ee3e48e2fb0e6a7
Alexander Yashin <alexandr.yashin@emlid.com>
2019-10-21 03:03:36 +0300

    main: add sort entry-point

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-#include "salolib/include/quicksort.hpp"
+#include "hwlib/include/sort.hpp"
+
+
+template <typename T>
+void print_array(const T* first, const T* last)
+{
+    for (int i=0; first+i<=last; i++)
+        std::cout << *(first+i) << " ";
+    std::cout << "\n";
+}
+
+
+void _sort(int * arr, int size)
+{
+    bool (*comparator_ptr)(int, int) = forward_comparator;
+    sort(arr, arr + size - 1, comparator_ptr);
+    print_array(arr, arr + size - 1);
+    return;
+}
+
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello, World!" << quicksort() <<std::endl;
-	return 0;
+    int array[] = {7, 2, 2, 2, 1, 5, 5, 5, 0};
+    _sort(array, sizeof (array)/ sizeof (int));
+    return 0;
 }
